73d86dbae1faf0fee8d62f80e5d909d3835c57e3
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-05-02 00:24:45 -0300

    Criação de um novo menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 window.alert(`Seja bem vindo ao meu mundo digital`)   
 /*var nome = window.prompt(`Qual é seu nome ?`)
 window.alert('É um grande prazer em te conhecer, ' + nome)*/
+
+      const menu = document.querySelector('.menu');
+      window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 100) {
+          menu.style.backgroundColor = '#111';
+        } else {
+          menu.style.backgroundColor = '#444';
+        }
+      });
+   
