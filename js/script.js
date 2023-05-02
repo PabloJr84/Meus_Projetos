25fc7f3e6c5371edd3cc4641c7a22fa996eeb084
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-05-02 01:27:21 -0300

    Atualização da pagina principal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-window.alert(`Seja bem vindo ao meu mundo digital`)   
+/*window.alert(`Seja bem vindo ao meu mundo digital`)  */ 
 /*var nome = window.prompt(`Qual é seu nome ?`)
 window.alert('É um grande prazer em te conhecer, ' + nome)*/
-
+/*
       const menu = document.querySelector('.menu');
       window.addEventListener('scroll', () => {
         if (window.pageYOffset > 100) {
@@ -10,4 +10,4 @@ window.alert('É um grande prazer em te conhecer, ' + nome)*/
           menu.style.backgroundColor = '#444';
         }
       });
-   
+   */
